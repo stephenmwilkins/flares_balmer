55113ceac4c6830bc39947be07207f3cdbc39836
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-04-21 08:08:05 +0100

    updated

--- analysis/predictions/M.py
+++ analysis/predictions/M.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import matplotlib.cm as cm
+import cmasher as cmr
 
 import scipy.stats as stats
 
@@ -17,7 +18,7 @@ import flares_utility.plt
 import flares_utility.analyse as analyse
 import flares_utility.stats
 
-x_limits = [8.5, 11.5]
+x_limits = [8.01, 11.49]
 # x_limits = [28.01, 30.49]
 
 filename = '/Users/stephenwilkins/Dropbox/Research/data/simulations/flares/flares_noparticlesed.hdf5'
@@ -71,11 +72,41 @@ limits[x] = x_limits
 # cmap = cmr.get_sub_cmap('cmr.sapphire', 0.4, 1.0)
 cmap = cmr.bubblegum
 
-fig, axes = flares_utility.plt.linear_redshift(
-    D, zeds, x, y, s, limits=limits, scatter_colour_quantity=z, scatter_cmap=cmap, bins=15, rows=2, lowz=True, add_weighted_range=False)
+# fig, axes = flares_utility.plt.linear_redshift(
+#     D, zeds, x, y, s, limits=limits, scatter_colour_quantity=z, scatter_cmap=cmap, bins=15, rows=2, lowz=True, add_weighted_range=False)
 
-# for ax in axes.flatten():
-#     ax.set_yscale('log')
+fig, axes = flares_utility.plt.linear_redshift(
+    D, zeds, x, y, s, limits=limits, scatter=False, bins=15, rows=2, lowz=True, add_weighted_range=True)
+
+
+flaxes = axes.flatten()
+
+# D4000 = [1.05876971, 0.00664352
+# Dsteve = [1.76259398, 0.01156703]
+
+
+colors = cmr.take_cmap_colors('cmr.infinity', 5, cmap_range=(0.15, 0.85))
+
+x = 10.61
+xerr = 0.02
+y = np.log10(1.76*(4200**2/3500**2))
+print(y)
+yerr = np.log10(1.76+0.01156703)-np.log10(1.76)
+
+flaxes[5].errorbar(x, y, xerr=xerr, yerr=yerr, fmt='o',
+                   label=r'$\rm Carnall+23$', c=colors[0], markersize=3, elinewidth=1)
+z = 7.3
+x = 8.7
+xerr = 0.1
+y = 0.311
+yerr = 0.028
+
+flaxes[3].errorbar(x, y, xerr=xerr, yerr=yerr, fmt='o',
+                   label=r'$\rm Looser+23$', c=colors[1], markersize=3, elinewidth=1)
+
+
+for ax in axes.flatten():
+    ax.legend(fontsize=8)
 
 
 fig.savefig(f'figs/M.pdf')
